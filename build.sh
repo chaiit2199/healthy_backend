35d1de6941ad09be42a204a6109ad324958b801a
chaiit2199 <chaiit2199@gmail.com>
2025-04-19 23:46:03 +0700

    update script build

--- build.sh
+++ build.sh
@@ -1,24 +1,20 @@
 # Remove the existing release directory and build the release
-sudo rm -rf "_build/prod"
+rm -rf "_build"
 
 #!/usr/bin/env bash
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
-# Copy dependencies' assets
-cd ../healthy_backend && mkdir -p priv/static/assets && rm mix.lock && mix deps.get && mix phx.copy default
-
-# Change mode to assets deploy local
-cd -
-mkdir -p priv/static/assets
-
-# Build assets for tailwind
-MIX_ENV=prod mix assets.deploy
+# * change mode to assets deploy local
 MIX_ENV=prod mix ua_inspector.download --force
+# sudo chown $USER -R ./priv/static
+MIX_ENV=prod mix assets.deploy
+
 # Release
 MIX_ENV=prod mix release
 if [ $? -eq 0 ]; then
+  # Only when mix release is successful
   build_date=$(date +%Y%m%d_%H%M)
   build_name=_build_healthy_backend_$build_date.tar.gz
   tar -czvf $build_name _build
