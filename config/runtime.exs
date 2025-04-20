29489d72d838a0120e4d23f2d2ffebc7ab5e2b9f
chaiit2199 <chaiit2199@gmail.com>
2025-04-20 17:04:15 +0700

    config prod env

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,9 +2,8 @@ import Config
 
 
 if config_env() == :prod do
-
   host = System.get_env("HOST") || "localhost"
-  http_port = System.get_env("HOST") || "localhost"
+  http_port = String.to_integer(System.get_env("PORT") || "8088")
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") || raise ("No SECRET_KEY_BASE config.")
@@ -28,5 +27,4 @@ if config_env() == :prod do
     url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: http_port],
     secret_key_base: secret_key_base
-
 end
