49c9c3bad2e60f4b8836fb8f0c1e3a9bb5f38733
chaiit2199 <chaiit2199@gmail.com>
2025-04-20 17:18:49 +0700

    update config gemini

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,8 +5,9 @@ if config_env() == :prod do
   host = System.get_env("HOST") || "localhost"
   http_port = String.to_integer(System.get_env("PORT") || "8088")
 
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") || raise ("No SECRET_KEY_BASE config.")
+  secret_key_base = System.get_env("SECRET_KEY_BASE") || raise ("No SECRET_KEY_BASE config.")
+  api_key_base = System.get_env("API_KEY_GEMINI") || raise ("No API_KEY_GEMINI config.")
+  url_gemini = System.get_env("URL_GEMINI") || raise ("No URL_GEMINI config.")
 
   config :healthy_backend, HealthyBackend.Repo,
     username: System.get_env("DB_USERNAME") || raise("No DB_USERNAME config."),
@@ -27,4 +28,9 @@ if config_env() == :prod do
     url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: http_port],
     secret_key_base: secret_key_base
+
+  config :healthy_backend,
+    env: config_env(),
+    API_KEY_GEMINI: api_key_base,
+    URL_GEMINI: url_gemini
 end
